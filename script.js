b774d9b7b55f37b3e045e65493f2bf0ac02f7b9b
elianyos <zorbacdt@gmail.com>
2026-02-11 14:54:36 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+fetch("data.json")
+  .then(response => response.json())
+  .then(data => {
+
+    const score = data.score;
+    const rotation = (score / 100) * 180;
+
+    document.getElementById("gaugeFill").style.transform =
+      `rotate(${rotation}deg)`;
+
+    document.getElementById("score").innerText = score;
+
+    let label = "";
+
+    if (score <= 25) label = "Extreme Fear";
+    else if (score <= 45) label = "Fear";
+    else if (score <= 55) label = "Neutral";
+    else if (score <= 75) label = "Greed";
+    else label = "Extreme Greed";
+
+    document.getElementById("label").innerText = label;
+  });
 
